b84e93b30e849a74b6d821702c579283620c31fa
elijah amoako <elijahamoako72@gmail.com>
2022-08-23 21:21:44 +0000

    updated

---
+++ temperatureConverter/Program.cs
@@ -0,0 +1,77 @@
+﻿Console.WriteLine("Welcome to my temperature converter app.");
+do
+{
+    Console.WriteLine("\nKindly choose an option below:\n");
+    Console.WriteLine("1. Celsius to Fahrenheit");
+    Console.WriteLine("2. Fahrenheit to Celsius");
+    Console.WriteLine("3. Celsius to Kelvin");
+    Console.WriteLine("4. Kelvin to Celsius");
+    Console.WriteLine("5. Fahrenheit to Kelvin");
+    Console.WriteLine("6. Kelvin to Fahrenheir\n");
+
+    string option = Console.ReadLine();
+
+
+    switch (option)
+    {
+
+        case "1":
+            Console.Write("\nEnter the temperature in Celsius: ");
+            double celsiusTemp = Convert.ToDouble(Console.ReadLine());
+
+            double fahrenheitTemp = (celsiusTemp / 5) * 9 + 32;
+            Console.WriteLine(fahrenheitTemp);
+            break;
+
+        case "2":
+            Console.Write("\nEnter the temperature in Fahrenheit: ");
+            fahrenheitTemp = Convert.ToDouble(Console.ReadLine());
+
+            celsiusTemp = (fahrenheitTemp - 32) * 5 / 9;
+            Console.WriteLine(celsiusTemp);
+            break;
+
+        case "3":
+            Console.Write("\nEnter the temperature in Celsius: ");
+            celsiusTemp = Convert.ToDouble(Console.ReadLine());
+
+            double kelvinTemp = celsiusTemp + 273.15;
+            Console.WriteLine(kelvinTemp);
+            break;
+
+        case "4":
+            Console.Write("\nEnter the temperature in Kelvin: ");
+            kelvinTemp = Convert.ToDouble(Console.ReadLine());
+
+            celsiusTemp = kelvinTemp - 273.15;
+            Console.WriteLine(celsiusTemp);
+            break;
+
+        case "5":
+            Console.Write("\nEnter the temperature in Fahrenheit: ");
+            fahrenheitTemp = Convert.ToDouble(Console.ReadLine());
+
+            kelvinTemp = (fahrenheitTemp + 459.67) * 5 / 9;
+            Console.WriteLine(kelvinTemp);
+            break;
+
+        case "6":
+            Console.Write("\nEnter the temperature in Kelvin: ");
+            kelvinTemp = Convert.ToDouble(Console.ReadLine());
+
+            fahrenheitTemp = (kelvinTemp * 9) / 5 - 459.67;
+            Console.WriteLine(fahrenheitTemp);
+            break;
+
+        default:
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid option.");
+            Console.ResetColor();
+            Console.WriteLine("Choose a valid option.");
+            break;
+    }
+    Console.WriteLine("Do you want to make another conversion? Y / N");
+} while (Console.ReadLine().ToUpper() == "Y");
+Console.WriteLine("Bye!");
+
+
